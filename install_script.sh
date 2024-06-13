bf9b0a9415f1a5fca76b94907f983d7fe517f7d2
fshangala <fshangala@gmail.com>
2024-06-13 10:36:02 +0200

    created install script

--- install_script.sh
+++ install_script.sh
@@ -1,28 +1,11 @@
-# update system packages
-sudo apt update
-
-# check if git is installed
-git --version
-
-# install git
-sudo apt install git
-
-# check if nginx is installed
-nginx -v
-
-# install nginx
-sudo apt install nginx
-
-# check if python is installed
-python3 --version
-
-# install python
-sudo apt install python3
+# Installation requirements
+# - git
+# - python3
 
 # download the control panel
-cd ~/
+cd /
 git clone https://github.com/fshangala/power-server-control-panel.git
-cd ~/power-server-control-panel
+cd /power-server-control-panel
 
 # create a virtual environment
 python3 -m venv env
@@ -34,10 +17,12 @@ cp .env-production .env
 python manage.py migrate
 python manage.py createsuperuser --no-input
 
-# start and enable the service
-sudo systemctl start gunicorn.socket
-sudo systemctl enable gunicorn.socket
+# install the service
+cp ./power-server-cp.service /etc/systemd/system/power-server-cp.service
 
-# configure nginx to proxy pass our server then 
-sudo ufw delete allow 8000
-sudo ufw allow 'Nginx Full'
+# start and enable the service
+sudo systemctl start power-server-cp.service
+sudo systemctl enable power-server-cp.service
+
+# configure firewall to allow server port
+sudo ufw allow 8000
