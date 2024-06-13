63056926b34fa5c4eed81af843150d61f3db72b8
fshangala <fshangala@gmail.com>
2024-06-13 10:46:34 +0200

    updated the install script

--- install_script.sh
+++ install_script.sh
@@ -3,26 +3,25 @@
 # - python3
 
 # download the control panel
+echo "\n\nDownloading the control panel...\n"
 cd /
 git clone https://github.com/fshangala/power-server-control-panel.git
 cd /power-server-control-panel
 
 # create a virtual environment
+echo "\n\n Creating and activating a virtual environment\n"
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 cp .env-production .env
 
 # run migrations and create a default superuser
+echo "\n\nRunning migrations and creating a super user\n"
 python manage.py migrate
 python manage.py createsuperuser --no-input
 
 # install the service
+echo "\n\nInstalling and running the service\n"
 cp ./power-server-cp.service /etc/systemd/system/power-server-cp.service
-
-# start and enable the service
 sudo systemctl start power-server-cp.service
 sudo systemctl enable power-server-cp.service
-
-# configure firewall to allow server port
-sudo ufw allow 8000
